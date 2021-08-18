d063dd6e48d071ee7b984346ae430d65a27544a0
jmsche <contact@jmsche.fr>
2021-08-18 16:24:31 +0200

    Test dashboard

--- tests/Controller/AbstractControllerTest.php
+++ tests/Controller/AbstractControllerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -14,5 +15,17 @@ abstract class AbstractControllerTest extends WebTestCase
     protected function setUp(): void
     {
         static::$client = static::createClient();
+    }
+
+    protected function loginAsUser1(): void
+    {
+        $this->login(1);
+    }
+
+    private function login(int $userId): void
+    {
+        $user = self::$client->getContainer()->get('doctrine.orm.entity_manager')
+            ->getRepository(User::class)->find($userId);
+        self::$client->loginUser($user);
     }
 }
