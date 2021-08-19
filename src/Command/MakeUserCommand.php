0f77077191cbf9becc8b2a8e575edc3da86a210b
jmsche <contact@jmsche.fr>
2021-08-19 10:14:14 +0200

    One role per user, improve make-user command and ask for role

--- src/Command/MakeUserCommand.php
+++ src/Command/MakeUserCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Command;
 
 use App\Entity\User;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -12,6 +13,9 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[AsCommand(
     name: 'app:make-user',
@@ -22,6 +26,7 @@ final class MakeUserCommand extends Command
     public function __construct(
         private UserPasswordHasherInterface $hasher,
         private EntityManagerInterface $manager,
+        private ValidatorInterface $validator,
     ) {
         parent::__construct();
     }
@@ -30,15 +35,41 @@ final class MakeUserCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $email = $io->ask('Email address');
-        $plainPassword = $io->askHidden('Password');
+        $email = $io->ask('Email address', null, function ($email): string {
+            $errors = $this->validator->validate($email, [new Assert\NotBlank(), new Assert\Email()]);
+
+            if (0 < \count($errors)) {
+                $combinedErrors = array_map(fn (ConstraintViolationInterface $error) => $error->getMessage(), iterator_to_array($errors));
+                throw new \RuntimeException(implode("\n", $combinedErrors));
+            }
+
+            return $email;
+        });
+        $plainPassword = $io->askHidden('Password', function ($password): string {
+            $errors = $this->validator->validate($password, [new Assert\NotBlank()]);
+
+            if (0 < \count($errors)) {
+                $combinedErrors = array_map(fn (ConstraintViolationInterface $error) => $error->getMessage(), iterator_to_array($errors));
+                throw new \RuntimeException(implode("\n", $combinedErrors));
+            }
+
+            return $password;
+        });
+        $role = $io->choice('Role', User::getAvailableRoles(), 'ROLE_USER');
 
         $user = new User();
         $user->setEmail($email)
-            ->setPassword($this->hasher->hashPassword($user, $plainPassword));
+            ->setPassword($this->hasher->hashPassword($user, $plainPassword))
+            ->setRole($role);
 
-        $this->manager->persist($user);
-        $this->manager->flush();
+        try {
+            $this->manager->persist($user);
+            $this->manager->flush();
+        } catch (UniqueConstraintViolationException) {
+            $io->error(sprintf('Email address %s is already used.', $email));
+
+            return Command::FAILURE;
+        }
 
         $io->success(
             sprintf('User %s has been successfully created! You can now log in.', $email)
---
+++ tests/Command/MakeUserCommandTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Command;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Exception\MissingInputException;
+use Symfony\Component\Console\Tester\CommandTester;
+
+final class MakeUserCommandTest extends KernelTestCase
+{
+    public function testExecute(): void
+    {
+        $kernel = self::createKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('app:make-user');
+        $commandTester = new CommandTester($command);
+        $commandTester->setInputs(['contact@test.com', 'test', 'ROLE_ADMIN']);
+
+        $commandTester->execute(['command' => $command->getName()]);
+        $output = $commandTester->getDisplay();
+        self::assertStringContainsString('User contact@test.com has been successfully created! You can now log in.', $output);
+
+        $commandTester->execute(['command' => $command->getName()]);
+        $output = $commandTester->getDisplay();
+        self::assertStringContainsString('Email address contact@test.com is already used.', $output);
+    }
+
+    /**
+     * @dataProvider provideInvalidCases
+     */
+    public function testExecuteWithInvalidInput(string $email, string $password, string $role): void
+    {
+        $kernel = self::createKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('app:make-user');
+        $commandTester = new CommandTester($command);
+        $commandTester->setInputs([$email, $password, $role]);
+
+        $this->expectException(MissingInputException::class);
+        $commandTester->execute(['command' => $command->getName()]);
+    }
+
+    public function provideInvalidCases(): iterable
+    {
+        yield 'no_email' => ['', '', ''];
+        yield 'invalid_email' => ['contact', '', ''];
+        yield 'no_password' => ['contact@test.com', '', ''];
+    }
+}
