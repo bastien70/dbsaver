deaa4b59cfdc29ccc79533c166880df563b1a6b7
jmsche <contact@jmsche.fr>
2021-08-23 19:55:12 +0200

    Add some tests for DatabaseCrudController

--- tests/Controller/Admin/DatabaseCrudControllerTest.php
+++ tests/Controller/Admin/DatabaseCrudControllerTest.php
@@ -8,6 +8,50 @@ use App\Controller\Admin\DatabaseCrudController;
 
 final class DatabaseCrudControllerTest extends AbstractCrudControllerTest
 {
+    public function testEdit(): void
+    {
+        $url = $this->getActionUrl('edit', 1);
+
+        self::$client->request('GET', $url);
+        self::assertResponseRedirects('/');
+
+        $this->loginAsAdmin();
+        self::$client->request('GET', $url);
+        self::assertResponseStatusCodeSame(403);
+
+        $this->loginAsUser();
+        self::$client->request('GET', $url);
+        self::assertResponseIsSuccessful();
+    }
+
+    public function testShowDatabaseBackupsAction(): void
+    {
+        $url = $this->getActionUrl('showDatabaseBackupsAction', 1);
+
+        self::$client->request('GET', $url);
+        self::assertResponseRedirects('/');
+
+        $this->loginAsAdmin();
+        self::$client->request('GET', $url);
+        self::assertResponseStatusCodeSame(403);
+
+        $this->loginAsUser();
+        self::$client->request('GET', $url);
+        self::assertResponseRedirects();
+    }
+
+    public function testDelete(): void
+    {
+        $url = $this->getActionUrl('delete', 1);
+
+        self::$client->request('GET', $url);
+        self::assertResponseRedirects('/');
+
+        $this->loginAsUser();
+        self::$client->request('GET', $url);
+        self::assertResponseRedirects();
+    }
+
     protected function getControllerClass(): string
     {
         return DatabaseCrudController::class;
