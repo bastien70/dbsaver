e072134c2ccfc6966524a2c2b20a92203414d25b
jmsche <jonathan.scheiber@siep.be>
2021-09-03 09:09:03 +0200

    PostInstall command: add an option to only ask for missing params

--- src/Command/PostInstallCommand.php
+++ src/Command/PostInstallCommand.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use sixlive\DotenvEditor\DotenvEditor;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -19,43 +21,78 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 )]
 final class PostInstallCommand extends AbstractDotEnvCommand
 {
+    private SymfonyStyle $io;
+    private DotenvEditor $dotenvEditor;
+    private bool $onlyMissing;
+    private bool $anyValueWasUpdated = false;
+
     public function __construct(private ValidatorInterface $validator, private array $enabledLocales)
     {
         parent::__construct();
     }
 
+    protected function configure(): void
+    {
+        $this->addOption('only-missing', 'm', InputOption::VALUE_NONE, 'Only configure non existent parameters');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $editor = $this->getDotenvEditor($input);
+        $this->io = new SymfonyStyle($input, $output);
+        $this->dotenvEditor = $this->getDotenvEditor($input);
+        $this->onlyMissing = $input->getOption('only-missing');
 
-        $databaseUrl = $io->ask('Database URL (eg. "mysql://root:root@127.0.0.1:3306/dbsaver?serverVersion=5.7")', $this->getValueFromDotEnv($editor, 'DATABASE_URL'), function ($dsn) {
+        $this->ask('DATABASE_URL', 'Dztabase URL (eg. "mysql://root:root@127.0.0.1:3306/dbsaver?serverVersion=mariadb-10.6.2")', function ($dsn) {
             $this->validateInput($dsn, [new Assert\NotBlank()]);
 
             return $dsn;
         });
-        $mailerDsn = $io->ask('Mailer DSN (eg. "smtp://localhost")', $this->getValueFromDotEnv($editor, 'MAILER_DSN'), function ($dsn) {
+        $this->ask('MAILER_DSN', 'Mailer DSN (eg. "smtp://localhost")', function ($dsn) {
             $this->validateInput($dsn, [new Assert\NotBlank()]);
 
             return $dsn;
         });
-        $mailerSender = $io->ask('Mailer sender email (eg. "you@email.com")', $this->getValueFromDotEnv($editor, 'MAILER_SENDER'), function ($email) {
+        $this->ask('MAILER_SENDER', 'Mailer sender email (eg. "you@email.com")', function ($email) {
             $this->validateInput($email, [new Assert\NotBlank(), new Assert\Email()]);
 
             return $email;
         });
-        $defaultLocale = $io->choice('What locale should be the default one?', $this->enabledLocales, $this->getValueFromDotEnv($editor, 'DEFAULT_LOCALE'));
+        $this->choice('DEFAULT_LOCALE', 'What locale should be the default one?', $this->enabledLocales);
 
-        $editor->set('DATABASE_URL', $databaseUrl);
-        $editor->set('MAILER_DSN', $mailerDsn);
-        $editor->set('MAILER_SENDER', $mailerSender);
-        $editor->set('DEFAULT_LOCALE', $defaultLocale);
-        $editor->save();
+        $this->dotenvEditor->save();
         $this->removeDotEnvFileIfTest($input);
 
-        $io->success('Parameters have been saved in .env.local file.');
+        if ($this->anyValueWasUpdated) {
+            $this->io->success('Parameters have been saved in .env.local file.');
+        } else {
+            $this->io->success('No parameters were missing.');
+        }
 
         return Command::SUCCESS;
+    }
+
+    private function ask(string $key, string $question, callable $callback): void
+    {
+        $value = $this->getValueFromDotEnv($this->dotenvEditor, $key);
+        if (null === $value || !$this->onlyMissing) {
+            $value = $this->io->ask($question, $value, $callback);
+            $this->dotenvEditor->set($key, $value);
+            $this->anyValueWasUpdated = true;
+        }
+    }
+
+    private function choice(string $key, string $question, array $choices): void
+    {
+        $value = $this->getValueFromDotEnv($this->dotenvEditor, $key);
+        if (null === $value || !$this->onlyMissing) {
+            $value = $this->io->choice(
+                $question,
+                $choices,
+                $value,
+            );
+            $this->dotenvEditor->set($key, $value);
+            $this->anyValueWasUpdated = true;
+        }
     }
 
     private function validateInput(mixed $value, array $constraints): void
