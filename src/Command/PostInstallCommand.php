f14c0a27b81f53685f7c688fdddb79bbaf7c3104
jmsche <jonathan.scheiber@siep.be>
2021-09-03 14:54:04 +0200

    Fix typo

--- src/Command/PostInstallCommand.php
+++ src/Command/PostInstallCommand.php
@@ -42,7 +42,7 @@ final class PostInstallCommand extends AbstractDotEnvCommand
         $this->dotenvEditor = $this->getDotenvEditor($input);
         $this->onlyMissing = $input->getOption('only-missing');
 
-        $this->ask('DATABASE_URL', 'Dztabase URL (eg. "mysql://root:root@127.0.0.1:3306/dbsaver?serverVersion=mariadb-10.6.2")', function ($dsn) {
+        $this->ask('DATABASE_URL', 'Database URL (eg. "mysql://root:root@127.0.0.1:3306/dbsaver?serverVersion=mariadb-10.6.2")', function ($dsn) {
             $this->validateInput($dsn, [new Assert\NotBlank()]);
 
             return $dsn;
