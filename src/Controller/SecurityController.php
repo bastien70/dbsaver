4d33a9aa95c5fe53227f2c161d6bdbba403f7b89
jmsche <contact@jmsche.fr>
2021-10-03 13:59:41 +0200

    Clean up SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -19,43 +19,17 @@ class SecurityController extends AbstractController
             return $this->redirectToRoute('admin');
         }
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
         return $this->render('@EasyAdmin/page/login.html.twig', [
-            // parameters usually defined in Symfony login forms
-            'error' => $error,
-            'last_username' => $lastUsername,
-
-            // the title visible above the login form (define this option only if you are
-            // rendering the login template in a regular Symfony controller; when rendering
-            // it from an EasyAdmin Dashboard this is automatically set as the Dashboard title)
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+            'last_username' => $authenticationUtils->getLastUsername(),
             'page_title' => 'DbSaver',
-
-            // the string used to generate the CSRF token. If you don't define
-            // this parameter, the login form won't include a CSRF token
             'csrf_token_intention' => 'authenticate',
-
-            // the URL users are redirected to after the login (default: '/admin')
             'target_path' => $this->generateUrl('admin'),
-
-            // the label displayed for the username form field (the |trans filter is applied to it)
             'username_label' => new TranslatableMessage('security.login.email'),
-
-            // the label displayed for the password form field (the |trans filter is applied to it)
             'password_label' => new TranslatableMessage('security.login.password'),
-
-            // the label displayed for the Sign In form button (the |trans filter is applied to it)
             'sign_in_label' => new TranslatableMessage('security.login.submit'),
-
-            // the 'name' HTML attribute of the <input> used for the username field (default: '_username')
             'username_parameter' => 'email',
-
-            // the 'name' HTML attribute of the <input> used for the password field (default: '_password')
             'password_parameter' => 'password',
-
             'forgot_password_enabled' => true,
             'forgot_password_path' => $this->generateUrl('app_forgot_password_request'),
             'remember_me_enabled' => true,
