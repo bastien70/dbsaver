f59285f5153579180e33461badc90f0313f67b04
jmsche <contact@jmsche.fr>
2021-10-20 20:42:33 +0200

    Use DBAL types instead of strings

--- src/Entity/Traits/PrimaryKeyTrait.php
+++ src/Entity/Traits/PrimaryKeyTrait.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace App\Entity\Traits;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 trait PrimaryKeyTrait
 {
-    #[ORM\Id, ORM\GeneratedValue, ORM\Column(type: 'integer')]
+    #[ORM\Id, ORM\GeneratedValue, ORM\Column(type: Types::INTEGER)]
     private ?int $id = null;
 
     public function getId(): ?int
