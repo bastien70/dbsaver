26f10edb1775c9fb887529388f2894f1104c9b26
ToshY <31921460+ToshY@users.noreply.github.com>
2022-06-18 23:06:01 +0200

    added new query and criteria for correct removing of older backups

--- src/Repository/BackupRepository.php
+++ src/Repository/BackupRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Entity\Backup;
+use App\Entity\Database;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -19,5 +20,36 @@ class BackupRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Backup::class);
+    }
+
+    public function remove(Backup $backup)
+    {
+        $this->getEntityManager()->remove($backup);
+    }
+
+    public function flush()
+    {
+        $this->getEntityManager()->flush();
+    }
+
+    /**
+     * @return Backup[]
+     */
+    public function getActiveBackups(Database $database): array
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('b')
+            ->from(Backup::class, 'b')
+            ->innerJoin('b.database', 'd')
+            ->where(
+                $qb->expr()->eq(
+                    'd.id',
+                    $database->getId()
+                )
+            )
+            ->orderBy('b.createdAt', 'DESC')
+            ->setMaxResults($database->getMaxBackups() - 1);
+
+        return $qb->getQuery()->getResult();
     }
 }
