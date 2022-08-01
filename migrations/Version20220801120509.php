c2ca023d7f046924e547feb0ca4398cf00828aea
Bastien70 <bastien.loughin@outlook.com>
2022-08-01 16:31:18 +0200

    Updated migration file

--- migrations/Version20220801120509.php
+++ migrations/Version20220801120509.php
@@ -14,18 +14,16 @@ final class Version20220801120509 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Added backup custom periodicity support';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE `database` ADD backup_task_periodicity VARCHAR(255) NOT NULL, ADD backup_task_periodicity_number INT NOT NULL, ADD backup_task_start_from DATETIME NOT NULL, ADD backup_task_next_iteration DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE `database` DROP backup_task_periodicity, DROP backup_task_periodicity_number, DROP backup_task_start_from, DROP backup_task_next_iteration');
     }
 
