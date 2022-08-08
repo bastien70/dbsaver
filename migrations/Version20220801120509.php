185f29382e031a48288bed59a496b25407a4c062
Bastien <53140475+bastien70@users.noreply.github.com>
2022-08-08 08:46:43 +0200

    Update migrations/Version20220801120509.php
    Co-authored-by: jmsche <contact@jmsche.fr>

--- migrations/Version20220801120509.php
+++ migrations/Version20220801120509.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20220801120509 extends AbstractMigration
 {
     public function getDescription(): string
