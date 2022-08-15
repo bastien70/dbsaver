f1a3e7209788a851d6c7fbd6095ee500bd22421d
bastien70 <bastien.loughin@outlook.com>
2022-08-15 10:44:19 +0200

    Updated migration

--- migrations/Version20220801120509.php
+++ migrations/Version20220801120509.php
@@ -17,7 +17,9 @@ final class Version20220801120509 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE `database` ADD backup_task_periodicity VARCHAR(255) NOT NULL DEFAULT :periodicity, ADD backup_task_periodicity_number INT NOT NULL DEFAULT 1, ADD backup_task_start_from DATETIME NOT NULL DEFAULT :start_from, ADD backup_task_next_iteration DATETIME NOT NULL DEFAULT :next_iteration', [
+        $this->addSql('ALTER TABLE `database` ADD backup_task_periodicity VARCHAR(255) NOT NULL, ADD backup_task_periodicity_number INT NOT NULL, ADD backup_task_start_from DATETIME NOT NULL, ADD backup_task_next_iteration DATETIME NOT NULL');
+        $this->addSql('UPDATE `database` SET backup_task_periodicity = :periodicity, backup_task_periodicity_number = :periodicity_number, backup_task_start_from = :start_from, backup_task_next_iteration = :next_iteration;', [
+            'periodicity_number' => 1,
             'periodicity' => BackupTaskPeriodicity::WEEK->value,
             'start_from' => (new \DateTime())->format('Y-m-d'),
             'next_iteration' => (new \DateTime('+1 week'))->format('Y-m-d'),
